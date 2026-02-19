bd775723c5247e8ea0678bea4bbd8324dc03ceb1
tikrack <tikrackcode@gmail.com>
2026-02-19 19:00:50 +0330

    checks

---
+++ electron/db/migrations/20260219153027_create_checks_table.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { import("knex").Knex.SchemaBuilder }
+ */
+exports.up = function(knex) {
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { import("knex").Knex.SchemaBuilder }
+ */
+exports.down = function(knex) {
+  
+};
