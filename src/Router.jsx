0a4799e990d3af2ea599cfe012815b0fc94bd410
tikrack <tikrackcode@gmail.com>
2026-02-19 14:50:35 +0330

    adding user list

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
 import MainLayout from "./layouts/MainLayout.jsx";
 import HomeTemplate from "./templates/HomeTemplate.jsx";
-import CheckIssueTemplate from "./templates/CheckIssueTemplate.jsx";
-import {useEffect} from "react";
+import UserListPage from "./modules/users/page/UserListPage";
 
 const Router = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomeTemplate />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (<>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<MainLayout/>}>
+                        <Route path="/" element={<HomeTemplate/>}/>
+                        <Route path={"users"}>
+                            <Route index element={<UserListPage/>}/>
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>);
 };
 
 export default Router;
---
+++ src/modules/users/page/UserListPage.jsx
@@ -0,0 +1,11 @@
+import Container from "../../../components/layout/Container";
+
+const UserListPage = () => {
+    return <>
+        <Container>
+
+        </Container>
+    </>
+}
+
+export default UserListPage
