2c689ce3594df9870cebc6ed501194ed1c4a0579
tikrack <tikrackcode@gmail.com>
2026-02-19 14:06:02 +0330

    finish databse and start develop frontent

--- src/templates/CheckIssueTemplate.jsx
+++ src/templates/CheckIssueTemplate.jsx
@@ -1,11 +1,15 @@
 import Header from "../components/layout/Header.jsx";
 import Container from "../components/layout/Container.jsx";
+import Form from "../components/form/Form";
+import Input from "../components/form/Input";
 
 const CheckIssueTemplate = () => {
   return (
     <>
       <Container className={"pt-10"}>
-
+        <Form>
+            <Input name={"Hel"}/>
+        </Form>
       </Container>
     </>
   );
