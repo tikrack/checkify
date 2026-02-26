c5753cbfba0687141f4af13e397b6d9b305d0c9c
tikrack <tikrackcode@gmail.com>
2026-02-26 14:46:09 +0330

    index.html

---
+++ electron/db/seeds/users_seeder.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  await knex('users').del()
+  await knex('users').insert([
+    {
+      id: 1,
+      name: "علی",
+      family: "گیاهی"
+    },
+  ]);
+};
